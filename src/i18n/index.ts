96abf90b11459e62b28da2da0562a5fa36682e66
steve.dao <daoduyquang91@gmail.com>
2025-05-01 17:36:48 +0800

    fix vn

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -20,7 +20,7 @@ i18n
   // init i18next
   .init({
     resources,
-    fallbackLng: 'en',
+    fallbackLng: 'vi',
     debug: process.env.NODE_ENV === 'development',
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
